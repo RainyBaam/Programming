736d773eb928271154934746eeb40574b782b9d1
IvanovIvan <ivanovivanand7@gmail.com>
2022-11-15 16:19:46 +0300

    done

--- 3term/main.py
+++ 3term/main.py
@@ -1,20 +1,33 @@
 import math
+import calc_print
+
+PARAMS = {'precision': None,
+        'output_type': None,
+        'possible_types': None,
+        'dest': None}
 
 
-def main():
+def load_params(file="params.ini"):
     """
-     Функция main позволяет ввести значения с клавиатуры
-     и запустить вычисление действия калькулятора
+        Функция записи параметров из файла в словарь PARAMS
+        На вход поступает имя файла, в процессе выполнения
+        Параметры построчно записываются в PARAMS.
     """
-    operand1 = float(input("Введите 1 число:"))
-    operand2 = float(input("Введите 2 число:"))
-    action = input("Введите действие: ")
-    print(type(operand1), type(operand2))
-    result = calculate(operand1, operand2, action, settings.get('precision'))
-    print("Результат вычисления:", result)
+    global PARAMS
+    try:
+        f = open(file, mode='r', errors='strict')
+        lines = f.readlines()
+        for l in lines:
+            param = l.split('=')
+            param[1] = param[1].strip('\n')
 
+            if param[0] != 'dest':
+                param[1] = eval(param[1])
 
-settings = {'precision': '0.00001'}
+            PARAMS[param[0]] = param[1]
+    except FileNotFoundError:
+        print("Файл с параметрами не найден")
+        sys.exit()
 
 
 def convert_precision(precision):
@@ -47,12 +60,12 @@ def convert_precision_alt(**kwargs):
             return i
 
 
-def standard_deviation(*args, precision=None):
+def standard_deviation(*args, **kwargs):
     """
     Функция вычисляет среднеквадратическое отклонение из
     произвольного числа аргументов в виде кортежа
     """
-    precision = convert_precision(precision)
+    precision = convert_precision(kwargs['precision'])
     arith_mean = 0
     sum_squares = 0
     for arg in args:
@@ -64,60 +77,112 @@ def standard_deviation(*args, precision=None):
     return round(deviation, precision)
 
 
+def user_input():
+    args = []
+    values_counter = input("Введите кол-во значений для вычисления: ")
+    for counter in range(0, int(values_counter)):
+        value = input("Введите значение: ")
+        try:
+            if value == "":
+                break
+            value = float(value)
+        except ValueError:
+            print("Введите число в правильном формате (разделитель дробной части '.'")
+        else:
+            args.append(value)
+
+    print(args)
+    if len(args) <= 1:
+        return "Недостаточно аргументов для вычисления"
+
+    print("Введите действие \n"
+          "Доступные действия: +, -, *, /, ^, //, %")
+    action = input()
+    result = calculate(*args, action, **PARAMS)
+    calc_print.print_results(*args, action=action, result=result)
+    calc_print.write_log(*args, action=action, result=result, file=PARAMS['dest'])
+
 def digits_round(result, precision):
     """
     Функция позволяет округлять вещественные числа 
     по точности precision
     """
     if type(result) == float and not result.is_integer():
-        precision = convert_precision(precision)
         result = round(result, precision)
         return result
     else:
         return result
 
 
-def calculate(op1, op2, act, precision=None):
+def calculate(*args, **kwargs):
     """
     Просто калькулятор)
     """
+    precision = convert_precision(kwargs['precision'])
+    output_type = kwargs['output_type']
+    act = args[-1]
+
     if act == "+":
-        res = op1 + op2
+        res = sum(args[0:len(args) - 1])
+
     elif act == "-":
-        res = op1 - op2
+        res = args[0]
+        for n in args[1:len(args) - 1]:
+            res -= n
+
     elif act == "*":
-        res = op1 * op2
-        if precision:
-            res = digits_round(res, precision)
+        res = 1
+        for n in args[0:len(args) - 1]:
+            res *= n
+
     elif act == "/":
-        if op2 != 0:
-            res = digits_round(op1 / op2, precision)
-        else:
-            res = "Деление на ноль невозможно"
+        res = args[0]
+        for n in args[1:len(args) - 1]:
+            if n == 0:
+                print("Деление на ноль невозможно")
+                return None
+            res /= n
+
     elif act == "^":
-        if op2 % 1 == 0:
-            res = digits_round(op1 ** op2, precision)
-        else:
-            res = "Возведение в вещественную степень невозможно"
+        res = args[0]
+        for n in args[1:len(args) - 1]:
+            if n % 1 != 0:
+                print("Возведение в вещественную степень невозможно")
+                return None
+            res **= n
+
     # Целочисленное деление
     elif act == "//":
-        if op2 != 0:
-            res = op1 // op2
-        else:
-            res = "Деление на ноль невозможно"
+        res = args[0]
+        for n in args[1:len(args) - 1]:
+            if n == 0:
+                print("Деление на ноль невозможно")
+                return None
+            res //= n
+
     # Остаток от деления
     elif act == "%":
-        if op2 != 0:
-            res = digits_round(op1 % op2, precision)
-        else:
-            res = "Деление на ноль невозможно"
-    else:
-        res = "Операция не распознана"
+        res = args[0]
+        for n in args[1:len(args) - 1]:
+            if n == 0:
+                print("Деление на ноль невозможно")
+                return None
+            res %= n
 
+    else:
+        print("Операция не распознана")
+        return None
+
+    res = digits_round(res, precision)
     if isinstance(res, float) and res.is_integer():
         res = int(res)
-
     return res
 
 
-main()
+if __name__ == '__main__':
+    """
+         main позволяет ввести значения с клавиатуры
+         и запустить вычисление действия калькулятора
+    """
+    load_params()
+    user_input()
--- 3term/test.py
+++ 3term/test.py
@@ -1,24 +1,27 @@
 import unittest
+from main import *
 from two_sum import *
-from main import calculate, standard_deviation, convert_precision
 
+global PARAMS
+
+load_params()
 
 class MyTestCase(unittest.TestCase):
 
     def test_multiply(self):
-        self.assertEqual(calculate(1.0, 4, '*'), 4)  # Проверка операции умножения
+        self.assertEqual(calculate(1.0, 4, '*', **PARAMS), 4)  # Проверка операции умножения
 
     def test_sum(self):
-        self.assertEqual(calculate(1, 2, "+"), 3)
+        self.assertEqual(calculate(1, 2, "+", **PARAMS), 3)
 
     def test_divide(self):
-        self.assertEqual(calculate(7, 3, "//"), 2)
+        self.assertEqual(calculate(7, 3, '//', **PARAMS), 2)
 
     def test_mod(self):
-        self.assertEqual(calculate(9, 2, "%"), 1)
+        self.assertEqual(calculate(9, 2, "%", **PARAMS), 1)
 
     def test_standard_deviation(self):
-        self.assertEqual(standard_deviation(45, 45, 32, 65, 999, precision=0.00001), 381.04614)
+        self.assertEqual(standard_deviation(45, 45, 32, 65, 999, **PARAMS), 381.04614)
 
     def test_cp_with_1(self):
         self.assertEqual(convert_precision('0.1'), 1, "Должна быть 1")
@@ -32,7 +35,9 @@ class MyTestCase(unittest.TestCase):
     def test_cp_with_5_as_float(self):
         self.assertEqual(convert_precision(0.00001), 5, "Должно быть 5")
 
+
 class TwoSumTestCase(unittest.TestCase):
+
     def test_two_sum(self):
         self.assertEqual(two_sum(lst, target), (0, 6), "Базовый тестовый набор (test case)")
 
@@ -41,6 +46,7 @@ class TwoSumTestCase(unittest.TestCase):
 
     def test_two_sum3(self):
         self.assertEqual(two_sum(list(range(1000000)), 999999), (0, 999999))
+
 
 if __name__ == '__main__':
     unittest.main()
